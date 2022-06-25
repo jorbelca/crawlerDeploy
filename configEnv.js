e77298e066a5c39baeebba475133a5301fb95948
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-06-25 20:25:33 +0200

    Add files via upload

---
+++ configEnv.js
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+
+export const PORT = process.env.PORT
+export const MONGO = process.env.MONGO_URI
+export const SECRET = process.env.SECRET
+export const SALT_ROUNDS = Number(process.env.SALT_ROUNDS)
+export const MAIL_USERNAME = process.env.MAIL_USERNAME
+export const MAIL_PASSWORD = process.env.MAIL_PASSWORD
+export const MAIL_CLIENT_ID = process.env.MAIL_CLIENT_ID
+export const MAIL_CLIENT_SECRET = process.env.MAIL_CLIENT_SECRET
+export const MAIL_REFRESH_TOKEN = process.env.MAIL_REFRESH_TOKEN
