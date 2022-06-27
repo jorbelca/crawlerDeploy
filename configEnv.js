7ff846db96485b57220d4e5f6aa15deeb32bf145
jorbelca <76847923+jorbelca@users.noreply.github.com>
2022-06-27 11:14:00 +0200

    Add files via upload

--- configEnv.js
+++ configEnv.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 
-export const PORT = process.env.PORT
+export const PORT = process.env.PORT || 3030
 export const MONGO = process.env.MONGO_URI
 export const SECRET = process.env.SECRET
 export const SALT_ROUNDS = Number(process.env.SALT_ROUNDS)
